36033b2668f18d058acdc9daa89dad95f08e5644
ccwrc <j.muszelik@gmail.com>
2016-11-06 23:29:34 +0100

    logon.php favicon test

--- logon.php
+++ logon.php
@@ -69,7 +69,16 @@
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
 	
 	<script src="js/jquery-3.1.1.min.js"></script>
-    <script src="js/app.js"></script>	
+        <script src="js/app.js"></script>
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="apple-mobile-web-app-title" content="dziecioly">
+        <meta name="application-name" content="dziecioly">
+        <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
