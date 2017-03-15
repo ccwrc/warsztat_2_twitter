130546b17d543c881ad73ecbf2829db2120e6730
ccwrc <j.muszelik@gmail.com>
2017-03-15 17:22:04 +0100

    refactor - add bottom_menu_logoff

--- logon.php
+++ logon.php
@@ -129,19 +129,9 @@ Strona logowania ma mieć też link do strony tworzenia użytkownika. -->
         
       </div>
 
-
-      <div class ="footer">
-                  <br/><br/>
-                  <a href="<?=$linkSearch?><?=$linkWord?>" target="_blank">Ptasi los</a>
-                  <a href="http://www.lesnepogotowie.pl/" target="_blank">Leśne pogotowie</a>
-		  <a id="footerlink2" href="index.php">Dzięcioły</a> 
-		  <a href="logon.php">Logowanie</a> 
-		  <a href="create.php">Stwórz dziuplę</a> 
-		  <a id="footerlink5" href="showuser.php">Pokaż dzięcioła</a> 
-		  <a id="footerlink6" href="edituser.php">Edycja dziupli</a> 
-		  <a id="footerlink7" href="messages.php">Wiadomości</a> 
-		  <a id="footerlink8" href="logoff.php">Wyloguj</a> 
-      </div>      
+<?php
+include 'src/bottom_menu_logoff.php';
+?>
           
   </div>
 </body>
