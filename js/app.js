5156aa2f5183af0781604fb48fa720ef74ac2ff7
ccwrc <j.muszelik@gmail.com>
2017-03-15 17:56:56 +0100

    refactor - app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var divMainLogo = $('#mainBackLogo');
-  var smallImageDivMainLogo = $('#logoimage');
- 
-  var backLogoSound = new Audio('sound_sample/backlogosound.mp3');
-  var logoSound = new Audio('sound_sample/logosound.mp3');
+    var divMainLogo = $('#mainBackLogo');
+    var smallImageDivMainLogo = $('#logoimage');
+
+    var backLogoSound = new Audio('sound_sample/backlogosound.mp3');
+    var logoSound = new Audio('sound_sample/logosound.mp3');
 
     // start i stop dla dzwieku na belce gornej
-  divMainLogo.on('mouseenter', function() {
-    backLogoSound.play();
-  });
+    divMainLogo.on('mouseenter', function () {
+        backLogoSound.play();
+    });
 
-  divMainLogo.on('mouseleave', function() {
-    backLogoSound.pause();
-    backLogoSound.currentTime = 0;
-  });
-  
+    divMainLogo.on('mouseleave', function () {
+        backLogoSound.pause();
+        backLogoSound.currentTime = 0;
+    });
+
     // start i stop dla dzwieku na malym logo (gora, lewa strona)
-  smallImageDivMainLogo.on('mouseenter', function() {
-    logoSound.play();
-  });
+    smallImageDivMainLogo.on('mouseenter', function () {
+        logoSound.play();
+    });
 
-  smallImageDivMainLogo.on('mouseleave', function() {
-    logoSound.pause();
-    logoSound.currentTime = 0;
-  });
-
+    smallImageDivMainLogo.on('mouseleave', function () {
+        logoSound.pause();
+        logoSound.currentTime = 0;
+    });
 
 
 });
