a846cb6cfd9a57792f49907cc6819162cf42c067
ccwrc <j.muszelik@gmail.com>
2017-04-10 14:26:04 +0200

    connect.php - refactor

--- src/connect.php
+++ src/connect.php
@@ -7,16 +7,11 @@ function getDbConnection() {
     $DB_DATABASE = "las";
 
     $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+    $conn->query("SET CHARSET UTF8");
 
     if ($conn->connect_error) {
-       die("Brak połączenia z bazą danych, błąd: " . $conn->errno);
+        die("Brak połączenia z bazą danych, błąd: " . $conn->errno);
     }
 
-    $setEncodingSql = "SET CHARSET utf8"; //poprawne zwracanie pl znakow
-    $conn->query($setEncodingSql);
-
-    return $conn; //pamietac o zamknieciu po kazdej operacji (close+null)
+    return $conn;
 }
-
-?>
-
