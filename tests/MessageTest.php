3c3abb9a9da29a03f6907093aef415be31023323
ccwrc <j.muszelik@gmail.com>
2017-05-03 12:40:35 +0200

    add MessageTest.php

---
+++ tests/MessageTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../src/Message.php';
+
+class MessageTest extends PHPUnit_Extensions_Database_TestCase {
+    protected static $myConn;
+
+    public function getConnection() {
+        $conn = new PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+        $conn->query("SET GLOBAL FOREIGN_KEY_CHECKS=0");
+        return new PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection($conn, $GLOBALS['DB_NAME']);
+    }
+
+    public function getDataSet() {
+        return $this->createFlatXmlDataSet(__DIR__ . '/../datasets/message.xml');
+    }
+
+    public static function setUpBeforeClass() {
+        self::$myConn = new mysqli(
+                $GLOBALS['DB_HOST'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], $GLOBALS['DB_NAME']
+        );
+    }
+
+    // testy wlasciwe
+
+    public function testFalse() {
+        $this->assertFalse(false);
+    }
+    
+    
+
+}
